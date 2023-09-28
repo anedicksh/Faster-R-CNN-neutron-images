a82233a3b54ed107810c02ae964f6640f0e3f4bf
Ane Dicks Herrán <94798220+anedicksh@users.noreply.github.com>
2023-09-28 15:43:15 +0200

    Add files via upload

---
+++ src/config_2.py
@@ -0,0 +1,27 @@
+import torch
+
+BATCH_SIZE = 128 # increase / decrease according to GPU memeory
+RESIZE_TO = 512 # resize the image for training and transforms
+NUM_EPOCHS = 50 # number of epochs to train for
+NUM_WORKERS = 0
+
+DEVICE = torch.device('cpu') 
+  
+# Images and labels direcotry should be relative to train.py
+TRAIN_DIR_IMAGES = '../input/train_images'
+TRAIN_DIR_LABELS = '../input/train_xmls'
+VALID_DIR_IMAGES = '../input/valid_images'
+VALID_DIR_LABELS = '../input/valid_xmls'
+
+# classes: 0 index is reserved for background
+CLASSES = [
+    '__background__',
+    'Objects3D', 'Illegal']
+
+NUM_CLASSES = len(CLASSES)
+
+# whether to visualize images after creating the data loaders
+VISUALIZE_TRANSFORMED_IMAGES = False
+
+# location to save model and plots
+OUT_DIR = '../outputs/6'
